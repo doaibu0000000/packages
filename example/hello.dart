ce86936471a9de3c1b852c451178d6f0327fbae8
Doa Ibu <doa.ibu0000000@gmail.com>
2023-07-13 18:47:04 +0700

    packages

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:packages/hello.dart';
+
+void main(){
+  print(sayHello('Agung Ajalah'));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library packages;
+
+export 'package:packages/src/say_hello.dart';
+export 'package:packages/src/customer.dart' show Customer,Category,Product;
